a1530711c4d8a0354a49064712d4dd3454f678bf
mahovi4 <mmahovi4@gmail.com>
2026-04-15 19:07:58 +0300

    create 14.04.2026

---
+++ GKL Analise/GKL Analise/GKL Analise.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GKL_Analise
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.GKL_Analise = this.Factory.CreateRibbonGroup();
+            this.bScan = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.GKL_Analise.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.GKL_Analise);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // GKL_Analise
+            // 
+            this.GKL_Analise.Items.Add(this.bScan);
+            this.GKL_Analise.Label = "GKL Analise";
+            this.GKL_Analise.Name = "GKL_Analise";
+            // 
+            // bScan
+            // 
+            this.bScan.Label = "Сканировать";
+            this.bScan.Name = "bScan";
+            this.bScan.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.bScan_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.GKL_Analise.ResumeLayout(false);
+            this.GKL_Analise.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup GKL_Analise;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton bScan;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ GKL Analise/GKL Analise/GKL Analise.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GKL_Analise
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void bScan_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+    }
+}
